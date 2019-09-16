59c1d2359808bf38021f17b5f0391c640fad0c7d
Rupendra Kumar Dhiman <Rdhiman@BHAVNACORP.NET>
2019-09-16 13:31:22 +0530

    Change forgot password

--- src/components/ForgetPassword/ForgetForm.tsx
+++ src/components/ForgetPassword/ForgetForm.tsx
@@ -13,10 +13,10 @@ const ForgetForm = () => (
                 console.log(values);
             }}
         >
-            {({ errors, touched }) => (
-                <form className="user" autoComplete="true" noValidate>
+            {({ errors, touched, handleSubmit }) => (
+                <form className="user" autoComplete="true" onSubmit={handleSubmit} noValidate>
                     <h3 className="h4 text-gray-900 mb-4">Forget Password</h3>
-        <p>Please provide the Email ID that you have used when you signed up</p>
+                    <p>Please provide the Email ID that you have used when you signed up</p>
                     <div className="form-group">
                         <Field
                             component="input"
@@ -27,7 +27,7 @@ const ForgetForm = () => (
                             placeholder="Enter email address..."
                             autoComplete="off"
                             name="email" validate={validateEmail} />
-                        {errors.email && touched.email && <div className="main-error">{errors.email}</div>}
+                        <div className="error-box"><span className={"error " + (errors.email && touched.email ? "show" : "")}>{errors.email}</span></div>
                     </div>
                     <button type="submit" className="btn btn-primary btn-user btn-block" >Submit</button>
                 </form>
