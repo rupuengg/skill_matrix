5d64fc27570656a2a949bb5e6ba1960b43aa4b1a
Juhi <jbhardwaj@bhavnacorp.com>
2019-09-18 10:58:51 +0530

    Private route condition add

--- src/components/Common/PrivateRoute.tsx
+++ src/components/Common/PrivateRoute.tsx
@@ -7,18 +7,20 @@ import EmployeeLayout from '../layouts/employee.layout';
 let PrivateRoute = ({ component: Component, ...rest }: any) => (
   <Route {...rest} render={(props: any) => {
     const userString = localStorage.getItem('user');
-    const user = JSON.parse(userString ? userString : "");
-    console.log('localStorage.getItem(\'user\')', user);
-    if (Object.keys(user).length === 0) {
-      return <Redirect to={{ pathname: '/login' }} />
-    } else {
-      switch (user.user_type) {
-        case 'admin':
-          return <AdminLayout><Component {...props} /></AdminLayout>;
-        default:
-          return <EmployeeLayout><Component {...props} /></EmployeeLayout>;
+    if (userString) {
+      const user = JSON.parse(userString ? userString : "");
+      console.log('localStorage.getItem(\'user\')', user);
+      if (Object.keys(user).length === 0) {
+        return <Redirect to={{ pathname: '/login' }} />
+      } else {
+        switch (user.user_type) {
+          case 'admin':
+            return <AdminLayout><Component {...props} /></AdminLayout>;
+          default:
+            return <EmployeeLayout><Component {...props} /></EmployeeLayout>;
+        }
       }
-    }
+    } else return <Redirect to={{ pathname: '/login' }} />
   }} />
 );
 
